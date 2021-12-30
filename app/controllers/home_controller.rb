53f5316920594ddea5ec0d71dd5eea6799d0a42a
George Middleton <georgemiddleton3@gmail.com>
2021-12-29 21:23:31 -0700

    Redirect to root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
   def trash_em
     @trash_em = List.where(:completed => true).destroy_all
-    redirect_to lists_url, notice: 'All completed items were successfully deleted.'
+    redirect_to root_url, notice: 'All completed items were successfully deleted.'
   end
 
   def trash_em_all
     @trash_em_all = List.all.destroy_all
-    redirect_to lists_url, notice: 'All items were successfully deleted.'
+    redirect_to root_url, notice: 'All items were successfully deleted.'
   end
 
 end
